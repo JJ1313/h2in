c96aa13aed47df2d25dd79f0a6841cc976aae462
JJ <hgbeckerss@gmail.com>
2024-12-04 03:21:57 -0300

    Rediseno de menu con indicadores

--- js/app.js
+++ js/app.js
@@ -7,24 +7,61 @@ document.addEventListener('click', (event) => {
   }
 });
 
-const infoDiv = document.getElementById('infoDiv');
-infoDiv.addEventListener('click', (event) => {
-  // Open Popover
+const sideMenuLeft = document.getElementById('sideMenu-left');
+sideMenuLeft.addEventListener('click', (event) => {
+  // Manage  floating menu
+  let clickedElement;
+  if(event.target.tagName === 'H4'){
+    clickedElement = event.target.parentElement;
+  }
+  else{
+    clickedElement = event.target;
+  }
+  if(clickedElement.classList.contains('btn-sideMenuLeft')){
+    let idTarget = clickedElement.getAttribute('data-target');
+    clickedElement.toggleAttribute('data-active');
+    document.getElementById(idTarget).toggleAttribute('data-visible');
+  }
+});
+
+const sectionIndicadores = document.getElementById('section-indicadores')
+sectionIndicadores.addEventListener('click', (event) => {
+// Open Popover
   if(event.target.classList.contains('btn-popover')){
     let idTarget = event.target.getAttribute('data-target');
     document.getElementById(idTarget).toggleAttribute('data-visible', true);
+    setTimeout(() => {
+      document.getElementById(idTarget).toggleAttribute('data-visible', false);
+    }, 6000);
   }
-  // Manage accordion 
-  if(event.target.classList.contains('btn-accordion')){
-    let idTarget = event.target.getAttribute('data-target');
-    document.getElementById(idTarget).toggleAttribute('data-opened');
-    
+
+// Manage accordion
+if(event.target.classList.contains('btn-accordion')){
+  let idTarget = event.target.getAttribute('data-target');
+  document.getElementById(idTarget).toggleAttribute('data-expanded');
 }
 });
-
-const closeBtn = document.getElementById('closeBtn');
-closeBtn.addEventListener('click', () => {
-  infoDiv.toggleAttribute('data-visible', false);
+const closeBtnIndicadores = document.getElementById('btn-closeIndicadores');
+closeBtnIndicadores.addEventListener('click', () => {
+  sectionIndicadores.toggleAttribute('data-visible', false);
 });
-
+const sectionOpciones= document.getElementById('section-opciones')
+const closeBtnOpciones = document.getElementById('btn-closeOpciones');
+closeBtnOpciones.addEventListener('click', () => {
+  sectionOpciones.toggleAttribute('data-visible', false);
+});
+const selectDesaladora = document.getElementById('select-desaladoras');
+selectDesaladora.addEventListener('change', (event) => {
+  const desaladora = desaladoras[event.target.value];
+  document.getElementById('desaladora-status').innerText = desaladora.status;
+  document.getElementById('desaladora-capacity').innerText = desaladora.capacity;
+  document.getElementById('desaladora-unit').innerText = desaladora.capacityUnit;
+  document.getElementById('desaladora-use').innerText = desaladora.use;
+  document.getElementById('desaladora-owner').innerText = desaladora.owner;
+});
+const closeBtnDesaladora = document.getElementById('btn-closeDesaladoras');
+const sectionDesaladoras = document.getElementById('section-desaladoras');
+closeBtnDesaladora .addEventListener('click', () => {
+  sectionDesaladoras.toggleAttribute('data-visible', false);
+});
 
--- js/toggleSidebar.js
+++ js/toggleSidebar.js
@@ -1,8 +1,3 @@
-function toggleSidebar() {
-	var sidebar = document.getElementById('sidebar');
-	sidebar.classList.toggle('show-sidebar');
-}
-
 // COMUNAS
 function handleOption1(checkbox) {
     if (checkbox.checked) {
