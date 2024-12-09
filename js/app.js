cdca4a44ee4dd9d7795a382caeffb11ba5cbd5f2
JJ <hgbeckerss@gmail.com>
2024-12-09 14:57:30 -0300

    Menu left

--- js/app.js
+++ js/app.js
@@ -1,67 +1,50 @@
-document.addEventListener('click', (event) => {
-  if(!event.target.classList.contains('btn-popover')){
-    let openPopover = document.querySelector('.pop-info[data-visible]');
-    if(openPopover){
-      openPopover.removeAttribute('data-visible');
+const btnsSideMenu = document.querySelectorAll('.btn');
+btnsSideMenu.forEach(btn => {
+
+  btn.addEventListener('click', (event) => {
+    let idTarget;
+    if(event.target.tagName === 'H4' || event.target.tagName === "BUTTON"){
+      idTarget = event.target.parentElement.getAttribute('data-target');
     }
-  }
-});
-
-const sideMenuLeft = document.getElementById('sideMenu-left');
-sideMenuLeft.addEventListener('click', (event) => {
-  // Manage  floating menu
-  let clickedElement;
-  if(event.target.tagName === 'H4'){
-    clickedElement = event.target.parentElement;
-  }
-  else{
-    clickedElement = event.target;
-  }
-  if(clickedElement.classList.contains('btn-sideMenuLeft')){
-    let idTarget = clickedElement.getAttribute('data-target');
-    clickedElement.toggleAttribute('data-active');
+    else{
+      idTarget = event.target.getAttribute('data-target');
+    }
     document.getElementById(idTarget).toggleAttribute('data-visible');
-  }
+  });
 });
 
-const sectionIndicadores = document.getElementById('section-indicadores')
-sectionIndicadores.addEventListener('click', (event) => {
-// Open Popover
-  if(event.target.classList.contains('btn-popover')){
-    let idTarget = event.target.getAttribute('data-target');
+const btnsPopOver = document.querySelectorAll('.btn-popover');
+btnsPopOver.forEach(btn => {
+  btn.addEventListener('click', (event) => {
+    const idTarget = event.target.getAttribute('data-target');
     document.getElementById(idTarget).toggleAttribute('data-visible', true);
     setTimeout(() => {
       document.getElementById(idTarget).toggleAttribute('data-visible', false);
-    }, 6000);
-  }
+    }, 5000);
+  });
+});
 
-// Manage accordion
-if(event.target.classList.contains('btn-accordion')){
-  let idTarget = event.target.getAttribute('data-target');
-  document.getElementById(idTarget).toggleAttribute('data-expanded');
-}
+const btnsClose = document.querySelectorAll('.btn-close');
+btnsClose.forEach(btn => {
+  btn.addEventListener('click', (event) => {
+    const idTarget = event.target.getAttribute('data-target');
+    document.getElementById(idTarget).toggleAttribute('data-visible', false);
+  });
 });
-const closeBtnIndicadores = document.getElementById('btn-closeIndicadores');
-closeBtnIndicadores.addEventListener('click', () => {
-  sectionIndicadores.toggleAttribute('data-visible', false);
-});
-const sectionOpciones= document.getElementById('section-opciones')
-const closeBtnOpciones = document.getElementById('btn-closeOpciones');
-closeBtnOpciones.addEventListener('click', () => {
-  sectionOpciones.toggleAttribute('data-visible', false);
-});
+
 const selectDesaladora = document.getElementById('select-desaladoras');
 selectDesaladora.addEventListener('change', (event) => {
-  const desaladora = desaladoras[event.target.value];
-  document.getElementById('desaladora-status').innerText = desaladora.status;
-  document.getElementById('desaladora-capacity').innerText = desaladora.capacity;
-  document.getElementById('desaladora-unit').innerText = desaladora.capacityUnit;
-  document.getElementById('desaladora-use').innerText = desaladora.use;
-  document.getElementById('desaladora-owner').innerText = desaladora.owner;
+  document.getElementById("desaladora-status").innerText = desaladoras[selectDesaladora.value].status;
+  document.getElementById("desaladora-capacity").innerText = desaladoras[selectDesaladora.value].capacity;
+  document.getElementById("desaladora-unit").innerText = desaladoras[selectDesaladora.value].capacityUnit;
+  document.getElementById("desaladora-use").innerText = desaladoras[selectDesaladora.value].use;
+  document.getElementById("desaladora-owner").innerText = desaladoras[selectDesaladora.value].owner;
 });
-const closeBtnDesaladora = document.getElementById('btn-closeDesaladoras');
-const sectionDesaladoras = document.getElementById('section-desaladoras');
-closeBtnDesaladora .addEventListener('click', () => {
-  sectionDesaladoras.toggleAttribute('data-visible', false);
+
+document.getElementById('toggle-communes').addEventListener("change", (event) => {
+  toggleMarkers(communesMarkers, event.target.checked);
+});
+document.getElementById('toggle-desalination').addEventListener("change", (event) => {
+  toggleMarkers(desalinationMarkers, event.target.checked);
 });
 
