daa8baf3f44440ed2f546265901e1d440aa2d8e2
JJ <hgbeckerss@gmail.com>
2024-12-11 12:57:59 -0300

    corrige ortografia e indicadores NaN

--- js/desalinizadoras.js
+++ js/desalinizadoras.js
@@ -1,6 +1,6 @@
 const desaladoras =  {
-						'Desalinizadora Petorca': {"coordinates":[-32.4864007997886, -71.42715474742845], "status": "evaluacion", "owner": "Desala", "capacity": 1200, "capacityUnit": "L/s", "use": "Multiproposito"},
-						'Unidad 3 Ventanas': {"coordinates":[-32.750868732351655, -71.48341759177248], "status": "operando", "owner": "Electrica Ventanas SpA", "capacity": 28, "capacityUnit": "L/s", "use": "Industrial"},
-						'Unidad 4 Ventanas': {"coordinates":[-32.752289652927345, -71.48344929594408], "status": "operando", "owner": "Electrica Campiche Spa", "capacity": 30, "capacityUnit": "L/s", "use": "Industrial"},
-						'Aconcagua': {"coordinates":[-32.766888, -71.487533], "status": "construccion", "owner": "Aguas Pacifico S.A.", "capacity": 1000, "capacityUnit": "L/s", "use": "Multiproposito"},
+						'Desalinizadora Petorca': {"coordinates":[-32.4864007997886, -71.42715474742845], "status": "evaluación", "owner": "Desala", "capacity": 1200, "capacityUnit": "L/s", "use": "Multipropósito"},
+						'Unidad 3 Ventanas': {"coordinates":[-32.750868732351655, -71.48341759177248], "status": "operando", "owner": "Eléctrica Ventanas SpA", "capacity": 28, "capacityUnit": "L/s", "use": "Industrial"},
+						'Unidad 4 Ventanas': {"coordinates":[-32.752289652927345, -71.48344929594408], "status": "operando", "owner": "Eléctrica Campiche Spa", "capacity": 30, "capacityUnit": "L/s", "use": "Industrial"},
+						'Aconcagua': {"coordinates":[-32.766888, -71.487533], "status": "construcción", "owner": "Aguas Pacífico S.A.", "capacity": 1000, "capacityUnit": "L/s", "use": "Multipropósito"},
 };	
